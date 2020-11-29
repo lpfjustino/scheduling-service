abc54e52abfbbca6c25608566c1d49fbba3b430c
luisjustino <luis.justino@ifood.com.br>
2020-11-29 18:24:59 -0300

    Use lombok on DTO

--- src/main/java/com/jus10/schedule/dtos/AppointmentCreationDTO.java
+++ src/main/java/com/jus10/schedule/dtos/AppointmentCreationDTO.java
@@ -1,11 +1,13 @@
 package com.jus10.schedule.dtos;
 
+import lombok.Builder;
 import lombok.Data;
 
 import java.util.Date;
 import java.util.UUID;
 
 @Data
+@Builder
 public class AppointmentCreationDTO {
     private UUID clientId;
     private UUID serviceId;
